df500390790ef4bba87547ed7f63db72d7c80d4c
gjforfun <1273573899@qq.com>
2022-03-15 12:15:46 +0800

    first commit

---
+++ wavelet_faceformer/preprocess_fake.sh
@@ -0,0 +1,1 @@
+python data/preprosses.py --root /data/guojia/FFHQ/generated_image --dst /data/guojia/FFHQ/fake_wavelet --transform wavelet 
---
+++ wavelet_faceformer/preprocess_real.sh
@@ -0,0 +1,1 @@
+python data/preprosses.py --root /data/guojia/FFHQ/images1024x1024 --dst /data/guojia/FFHQ/real_wavelet --transform wavelet
